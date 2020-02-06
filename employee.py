c62ada24ed18f173fc319409534e00475551ec9b
lakshman <damerakd@amazon.com>
2020-02-06 14:16:34 -0500

    Modified employee script to add comments on when to use class methods and static methods

--- employee.py
+++ employee.py
@@ -14,5 +14,12 @@ emp_2 = Employee('bhavana','endreddy',50000)
 print(emp_1.email)
 print(emp_2.email)
 
+print('{} {}'.format(emp1.first, emp1.last))
+
 print(Employee.fullname(emp_1))
 print(emp_1.fullname())
+
+
+#class methods and static methods are used with help of @classmethod and @staticmethod decorators
+
+#static methods are used generally when we are not making use of self inside the method
